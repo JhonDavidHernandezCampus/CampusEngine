a2a215fc3e32ba033315da8db12ab1319764e88f
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-04-18 13:44:13 -0500

    feat: :sparkles: se creo la base de datos para los ogjetos que se agregan al modal

--- js/tienda.js
+++ js/tienda.js
@@ -1,6 +1,60 @@
 //script funcionalidad de carrito y tienda
-/* funciones contadoras y acumuladoras */
 let cantidad =0,cantPapas=0,cantPasteles=0,cantCafes=0,cont = 1 ;
+/* abase de datos */
+datos =[
+    {
+        imagen:"../images/papasmargarita.jpg",
+        titulo:"Papas",
+        precio:"2000",
+    },
+    {
+        imagen:"../images/Pastel-de-Pollo7.jpg",
+        titulo:"Pasteles",
+        precio:"2300",
+    },
+    {   
+        imagen:"../images/Color-del-cafe-filtrado-y-su-parecido-con-el-color-de-un-vino-tinto.-scaled.jpg",
+        titulo:"Cafe",
+        precio:"800",
+    },
+
+
+]
+datosAgregar = [
+    {
+        imagen:"../images/papasmargarita.jpg",
+        titulo:"Papas",
+        cantidad:cantPapas,
+    },    
+    {
+        imagen:"../images/Pastel-de-Pollo7.jpg",
+        titulo:"Pasteles",
+        cantidad:cantPasteles,
+    },   
+    {
+        imagen:"../images/Color-del-cafe-filtrado-y-su-parecido-con-el-color-de-un-vino-tinto.-scaled.jpg",
+        titulo:"Cafe",
+        cantidad:cantCafes,
+    },
+]
+
+/* agregar los elementos a html */
+
+const product = document.querySelector(".products");
+datos.map((e)=>{
+    product.insertAdjacentHTML("beforeend", `
+    <div class="carts">
+        <img src="${e.imagen}" alt="">
+        <h3>${e.titulo}</h3>
+        <p class="precioPapas">${e.precio}<d>$</d></p>
+        <a href="" id="${e.titulo}">AÑADIR AL CARRITO</a>
+    </div>
+    `)
+
+})
+
+
+/* funciones contadoras y acumuladoras */
 class productos{
     constructor({
         nombre="",
@@ -42,11 +96,17 @@ function cerrar(){
 
 
 /* eventos de los botones */
+const modal = document.querySelector("#modal-contenido");
+
+console.log(modal);
+console.log();
+
+
 
 const contador = document.querySelector("#cont");
-const aPapas = document.querySelector("#papas");
-const aPasteles= document.querySelector("#pasteles");
-const aCafe = document.querySelector("#cafe");  
+const aPapas = document.querySelector("#Papas");
+const aPasteles= document.querySelector("#Pasteles");
+const aCafe = document.querySelector("#Cafe");  
 
 
 
@@ -55,12 +115,23 @@ let btnAgregar=[aPapas,aPasteles,aCafe];
 btnAgregar.forEach((boton)=>{
     boton.addEventListener("click", (e)=>{
         e.preventDefault();
+        //(modal.querySelector(`${boton.id}`))?console.log("esta"):console.log("no esta");
+        modal.insertAdjacentHTML("beforeend", `
+        <div id="${boton.cafe}">
+            <div id="elemento">
+                <img src="../images/papasmargarita.jpg" alt="">
+                <h3>Papas</h3>
+                <p class="precioPapas">2000 <d>$</d></p>
+                <span>cantidad: 0</span>
+                <button id="restar">X</button>
+            </div>
+        </div>
+        <hr>`);
+
+        console.log(boton.id)
         contador.innerHTML=cont;
-        zona
         console.log(contador);
-        
         console.log(cont);
-
-
     });
 });
+
